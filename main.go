bd0e0a2b6b7c0e8a7f66e1aa64daf7dca1cd3d5d
alitaso345 <alice.maru345@gmail.com>
2020-03-20 13:02:33 +0900

    flag

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"strings"
 )
 
+var msg = flag.String("msg", "デフォルト値", "説明")
+var n int
+
+func init() {
+	flag.IntVar(&n, "n", 1, "回数")
+}
+
 func main() {
-	fmt.Println(os.Args)
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
 }
