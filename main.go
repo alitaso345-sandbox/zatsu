6c7ecd3fce49e0dc53b02efd49e9fc9ce8ffdeb2
alitaso345 <alice.maru345@gmail.com>
2020-03-20 13:33:42 +0900

    defer

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"strings"
-)
-
-var msg = flag.String("msg", "デフォルト値", "説明")
-var n int
-
-func init() {
-	flag.IntVar(&n, "n", 1, "回数")
-}
+import "fmt"
 
 func main() {
-	flag.Parse()
-	fmt.Println(strings.Repeat(*msg, n))
+	msg := "!!!"
+	defer fmt.Println(msg)
+	msg = "world"
+	defer fmt.Println(msg)
+	fmt.Println("hello")
 }
