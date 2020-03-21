a8e04fe036daa3daa9f40ceda2e99555d8b18d00
alitaso345 <alice.maru345@gmail.com>
2020-03-21 11:12:04 +0900

    type switch

--- main.go
+++ main.go
@@ -2,13 +2,51 @@ package main
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
+}
+
+type Octal int
+
+func (o Octal) String() string {
+	return fmt.Sprintf("%o", int(o))
+}
+
+type Binary int
+
+func (b Binary) String() string {
+	return fmt.Sprintf("%b", int(b))
+}
+
 type Func func() string
 
-func (f Func) String() string { return f() }
+func f(s Stringer) {
+	switch v := s.(type) {
+	case Hex:
+		fmt.Println(int(v), "Hex")
+	case Octal:
+		fmt.Println(int(v), "Octal")
+	case Binary:
+		fmt.Println(int(v), "Binary")
+	}
+}
 
 func main() {
-	var s fmt.Stringer = Func(func() string {
-		return "hi"
-	})
-	fmt.Println(s)
+	var s1 Stringer = Hex(100)
+	var s2 Stringer = Octal(100)
+	var s3 Stringer = Binary(100)
+
+	fmt.Println(s1.String())
+	fmt.Println(s2.String())
+	fmt.Println(s3.String())
+
+	f(Hex(100))
+	f(Octal(100))
+	f(Binary(100))
 }
