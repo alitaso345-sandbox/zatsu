fbf621b0539dd634c406a2bc9c677701a56396fd
alitaso345 <alice.maru345@gmail.com>
2020-03-21 11:04:40 +0900

    interface

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+type Hex int
+
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
+}
+
 func main() {
-	msg := "!!!"
-	defer fmt.Println(msg)
-	msg = "world"
-	defer fmt.Println(msg)
-	fmt.Println("hello")
+	var s Stringer = Hex(100)
+	fmt.Println(s.String())
 }
