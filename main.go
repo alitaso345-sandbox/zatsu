ebda9087afc17e4f2ce8d7d696dfa7c0b9616d11
alitaso345 <alice.maru345@gmail.com>
2020-03-22 11:43:50 +0900

    simplex

--- main.go
+++ main.go
@@ -6,14 +6,13 @@ func makeCh() chan int {
 	return make(chan int)
 }
 
-func recvCh(recv chan int) int {
-	go func() { recv <- 200 }()
+func recvCh(recv <-chan int) int {
 	return <-recv
 
 }
 
 func main() {
 	ch := makeCh()
-	go func() { ch <- 100 }()
+	go func(ch chan<- int) { ch <- 100 }(ch)
 	fmt.Println(recvCh(ch))
 }
