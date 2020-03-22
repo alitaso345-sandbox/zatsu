dd2e452cf168efcf08dc73c8e4adf84b649533fd
alitaso345 <alice.maru345@gmail.com>
2020-03-22 11:26:15 +0900

    select case

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	ch := make(chan int)
+	ch1 := make(chan int)
+	ch2 := make(chan string)
 
-	go func() {
-		ch <- 100
-	}()
+	go func() { ch1 <- 100 }()
+	go func() { ch2 <- "hi" }()
 
-	go func() {
-		v := <-ch
-		fmt.Println(v)
-	}()
-
-	time.Sleep(2 * time.Second)
+	select {
+	case v1 := <-ch1:
+		fmt.Println(v1)
+	case v2 := <-ch2:
+		fmt.Println(v2)
+	}
 }
